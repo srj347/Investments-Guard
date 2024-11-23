15e848aca06577aeb449cf6cf6163d4f6cf31b1a
E5000750 <suraj.verma@innovapptive.com>
2024-11-24 04:57:41 +0530

    Added shared viewmodel

---
+++ composeApp/src/androidMain/kotlin/com/guard/investments/viewmodels/AuthViewModel.kt
@@ -0,0 +1,10 @@
+package com.guard.investments.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.authentication.PhoneOtpAuthService
+
+class AuthViewModel: ViewModel() {
+    var phoneOtpAuthServices: PhoneOtpAuthService? = null
+
+
+}
